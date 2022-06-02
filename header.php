b7da622a038a087d4b36959fcde42fac21420dc5
Jackson Freitas <jacksonzitap.mc@gmail.com>
2022-06-01 22:24:43 -0300

    commit

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav d-flex mb-2 mb-lg-0">
-                    <div>
+                    
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="./">Home</a>
                         </li>
@@ -32,7 +32,7 @@
                         <li class="nav-item">
                             <a class="nav-link" href="./periodo.php">Periodo</a>
                         </li>
-                    </div>
+                    
                     <li class="nav-item">
                         <a class="nav-link" href="#">Gerência</a>
                     </li>

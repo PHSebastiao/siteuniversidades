6c5443f47d46f470d96343fee0defca4e7d8e050
Pedro Sebastiao <pedro.sebastiao@iatec.com>
2022-10-03 22:36:46 -0300

    home

--- header.php
+++ header.php
@@ -1,23 +1,29 @@
 <?php
 function genLinkClasses($linkFile)
 {
-    $currentFile = basename($_SERVER["SCRIPT_FILENAME"], '.php');
-    if ($linkFile == $currentFile) {
-        echo 'class="nav-link active" aria-current="page"';
-    } else {
-        echo 'class="nav-link"';
-    }
-    if ($linkFile != 'index') echo ' href="./' . $linkFile . '.php"';
+  $currentFile = basename($_SERVER["SCRIPT_FILENAME"], '.php');
+  if ($linkFile == $currentFile) {
+    echo 'class="nav-link active" aria-current="page"';
+  } else {
+    echo 'class="nav-link"';
+  }
+  if ($linkFile != 'index') echo ' href="./' . $linkFile . '.php"';
 }
 
 session_start();
 if ((!isset($_SESSION['login']) == true) && (!isset($_SESSION['senha']) == true)) {
-    header('Location: login.php');
+  header('Location: login.php');
 }
 ?>
+<style>
+  .container-custom {
+    display: flex;
+    padding: 0 3rem;
+  }
+</style>
 
 <header>
-<!-- <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+  <!-- <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
       <span class="fs-4">Sidebar</span>
@@ -69,56 +75,58 @@ if ((!isset($_SESSION['login']) == true) && (!isset($_SESSION['senha']) == true)
       </ul>
     </div>
   </div> -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="./">Site Universidades</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav d-flex mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('index') ?> href="./">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('universidades') ?>>Universidades</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('enderecos') ?>>Endereços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('pessoa') ?>>Pessoa</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('professor') ?>>Professores</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('aluno') ?>>Aluno</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('cursos') ?>>Cursos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('periodo') ?>>Período</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('disciplina') ?>>Disciplina</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('matriculas') ?>>Matrícula</a>
-                    </li>
-                    <li class="nav-item">
-                        <a <?php genLinkClasses('profcad') ?>>Cadastro Prof</a>
-                    </li>
-                    <!-- <li class="nav-item">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-custom">
+      <a class="navbar-brand" href="./">
+        <img src="assets\Logo.png" alt="Logo" width="20" height="30">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav d-flex mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a <?php genLinkClasses('index') ?> href="./">Home</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('universidades') ?>>Universidades</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('enderecos') ?>>Endereços</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('pessoa') ?>>Pessoa</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('professor') ?>>Professores</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('aluno') ?>>Aluno</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('cursos') ?>>Cursos</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('periodo') ?>>Período</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('disciplina') ?>>Disciplina</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('matriculas') ?>>Matrícula</a>
+          </li>
+          <li class="nav-item">
+            <a <?php genLinkClasses('profcad') ?>>Cadastro Prof</a>
+          </li>
+          <!-- <li class="nav-item">
                         <a class="nav-link" href="#">Gerência</a>
                     </li> -->
-                    <li class="nav-item">
-                        <?php echo '<a class="nav-link" href="logout.php?token='.md5(session_id()).'">Sair</a>'; ?>
-                        <!-- <a class="nav-link" href="logout.php?token='<?md5(session_id())?>'">Sair</a> -->
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+          <li class="nav-item">
+            <?php echo '<a class="nav-link" href="logout.php?token=' . md5(session_id()) . '">Sair</a>'; ?>
+            <!-- <a class="nav-link" href="logout.php?token='<? md5(session_id()) ?>'">Sair</a> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </header>
--- index.php
+++ index.php
@@ -8,14 +8,56 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/style.css">
     <title>Site Universidades</title>
+
+    <style>
+        .button-link a {
+            margin-bottom: 15px;
+            width: 125px;
+        }
+        .lista {
+            list-style-type: none;
+            padding: 0;
+        }
+        .linha {
+            display: flex;
+            gap:70px;
+        }
+        /* .coluna {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        } */
+    </style>
 </head>
 
 <body>
     <?php require_once 'header.php' ?>
     <main>
-        <div class="container my-5">
-            <h1>Site de Universidades</h1>
-            <p>Use a barra acima para ir para o gerenciador desejado</p>
+        <div class="container d-flex flex-column align-items-center my-5">
+            <div class="mb-5"><img src="assets\Site_Universidades.png"></div>
+            <h2></h2>
+            <div class="linha">
+                <div class="coluna">
+                    <ul class="lista">
+                        <li class="button-link"><a class="btn btn-secondary" href="./universidades.php">Universidades</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./enderecos.php">Endereços</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./pessoa.php">Pessoa</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./professor.php">Professores</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./aluno.php">Aluno</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./cursos.php">Cursos</a></li>
+                    </ul>
+                </div>
+                <div class="coluna">
+                    <ul class="lista">
+                        
+                        <li class="button-link"><a class="btn btn-secondary" href="./periodo.php">Período</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./disciplina.php">Disciplina</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./matricula.php">Matrícula</a></li>
+                        <li class="button-link"><a class="btn btn-secondary" href="./profcad.php">Cadastro Prof</a></li>
+                        <li class="button-link"><?php echo '<a class="btn btn-secondary" href="logout.php?token=' . md5(session_id()) . '">Sair</a>'; ?></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </main>
 </body>
